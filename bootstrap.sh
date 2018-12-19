7bdb72d0c99c9e57d29875acd768650ca0cb73b5
Till Kolditz <till.kolditz@gmail.com>
2018-12-20 00:59:04 +0100

    Add checks for MBEDTLSPATH. However, since I use 'locate' to check for existence of libmbedcrypto.a, this may fail when it is found in locations not searched for by ld.so
    Signed-off-by: Till Kolditz <till.kolditz@gmail.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,11 +3,11 @@
 function myexit() {
 	ret=$1
 	shift
-	echo "$@" >&2
+	echo "[ERROR] $@" >&2
 	exit $ret
 }
 
-echo "Checking for build tools 'cmake' and 'scons'. Using tools like 'head' and 'sed'."
+echo "[INFO] Checking for build tools 'cmake' and 'scons'. Using tools like 'head' and 'sed'."
 which cmake &>/dev/null
 ret=$?
 if ((ret == 0)); then
@@ -23,11 +23,11 @@ fi
 which scons &>/dev/null
 ret=$?
 if ((ret != 0)); then
-	echo "Could not find 'scons'! On ubuntu, you can install it via 'sudo apt install scons' to obtain the latest version."
+	myexit 4 "Could not find 'scons'! On ubuntu, you can install it via 'sudo apt install scons' to obtain the latest version."
 fi
 
 function checklib() {
-	echo -n "$1: "
+	echo -n "[CHECKLIB] $1: "
 	"$LOCATE" $1 &>/dev/null
 	ret=$?
 	if ((ret == 0)); then
@@ -42,7 +42,7 @@ function checklib() {
 	fi
 }
 
-echo -e "Trying to find required libraries with 'locate'.\nIf you have a system where this is done in a different way, then please adapt this script."
+echo -e "[INFO] Trying to find required libraries with 'locate'.\nIf you have a system where this is done in a different way, then please adapt this script."
 LOCATE=locate
 LIBS_TO_INSTALL=""
 checklib libhdf5.so libhdf5 libhdf5-dev
@@ -51,14 +51,41 @@ checklib libelf.so libelf libelf-dev
 checklib libconfig++.so libconfig libconfig++-dev
 
 if ((libhdf5 == 0)) || ((libxerces == 0)) || ((libelf == 0)) || ((libconfig == 0)); then
-	echo "Missing libraries${LIBS_TO_INSTALL}. Do you want to install them now? (you need sudo rights to continue)"
-	read -n 1 -p "[Y]es | [n]o" answer
+	echo "[QUESTION] Missing libraries${LIBS_TO_INSTALL}. Do you want to install them now? (you need sudo rights to continue)"
+	read -n 1 -p "[QUESTION] [Y]es | [n]o" answer
 	case "$answer" in
 		Y|y|\r|\n|"") sudo apt install ${LIBS_TO_INSTALL}; ;;
 		*) ;;
 	esac
 else
-	echo "Everything seems to be OK! :-) Trying to build..."
+	echo "[INFO] Everything seems to be OK! :-) Trying to build..."
+fi
+
+if [[ ! -z "$MBEDTLSPATH" ]]; then
+	echo "[INFO] MBEDTLSPATH set."
+	[[ -e "$MBEDTLSPATH" ]] || myexit 8 "MBEDTLSPATH is set, but the path it points to does not exist!"
+	LIBMEDCRYPTO_PATH=$("$LOCATE" "libmbedcrypto.a")
+	if [[ ! -z "${LIBMEDCRYPTO_PATH}" ]]; then
+		echo "[INFO] libmbedcrypto.a found at '${LIBMEDCRYPTO_PATH}'"
+	else
+		echo "[QUESTION] MBEDTLSPATH is set, but required static library libmedcrypto.a was not found. Should I initiate a build and install?"
+		echo "[QUESTION] This will create subfolder 'build' in the mbedtls path, then call 'cmake ..' and then 'sudo make -j$(nproc) install'."
+		read -n 1 -p "[QUESTION] [Y]es | [n]o" answer
+		case "$answer" in
+			Y|y|\r|\n|"")
+				pushd "$MBEDTLSPATH" &>/dev/null
+				mkdir -p build
+				pushd build &>/dev/null
+				cmake ..
+				sudo make -j$(nproc) install
+				popd &>/dev/null
+				popd &>/dev/null
+				;;
+			*) ;;
+		esac
+	fi
+else
+	echo "[INFO] MBEDTLSPATH not set."
 fi
 
 mkdir -p build || myexit 4 "Could not create directory 'build'"
